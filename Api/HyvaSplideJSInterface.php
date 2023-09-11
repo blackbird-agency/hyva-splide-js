2a1e5f7f038e630b15d77aa6d290bfe1abaa5267
Luderic Freymann <luderic@bird.eu>
2023-09-11 13:56:52 +0000

    fix: module name in several files

--- Api/HyvaSplideJSInterface.php
+++ Api/HyvaSplideJSInterface.php
@@ -6,7 +6,7 @@ namespace Blackbird\HyvaSplideJs\Api;
 
 interface HyvaSplideJSInterface
 {
-    const URL_SPLIDE_MIN_JS = 'Blackbird_HyvaSplideJS::js/splide.min.js';
-    const URL_SPLIDE_MIN_CSS = 'Blackbird_HyvaSplideJS::css/splide.min.css';
+    const URL_SPLIDE_MIN_JS = 'Blackbird_HyvaSplideJs::js/splide.min.js';
+    const URL_SPLIDE_MIN_CSS = 'Blackbird_HyvaSplideJs::css/splide.min.css';
     const EVENT_SPLIDE_LOADED = 'alpine-splide-js-loaded';
 }
--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Blackbird_HyvaSplideJS', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Blackbird_HyvaSplideJs', __DIR__);
