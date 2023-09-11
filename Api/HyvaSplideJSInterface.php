8d81a09bf60680e5f19f925c419327e6ff193d84
Luderic Freymann <luderic@bird.eu>
2023-09-11 11:11:18 +0000

    feat: include an interface to get the event

---
+++ Api/HyvaSplideJSInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blackbird\HyvaSplideJS\Api;
+
+interface HyvaSplideJSInterface
+{
+    const URL_SPLIDE_MIN_JS = 'Blackbird_HyvaSplideJS::js/splide.min.js';
+    const URL_SPLIDE_MIN_CSS = 'Blackbird_HyvaSplideJS::css/splide.min.css';
+    const EVENT_SPLIDE_LOADED = 'alpine-splide-js-loaded';
+}
