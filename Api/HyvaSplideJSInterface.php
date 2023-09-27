f0d50f556caff55ddeddd4a61ca6a26f05f5ecb8
Luderic Freymann <luderic@bird.eu>
2023-09-27 08:43:55 +0000

    fix: prevent splide for being load multiple time in the same page

--- Api/HyvaSplideJSInterface.php
+++ Api/HyvaSplideJSInterface.php
@@ -6,7 +6,5 @@ namespace Blackbird\HyvaSplideJs\Api;
 
 interface HyvaSplideJSInterface
 {
-    const URL_SPLIDE_MIN_JS = 'Blackbird_HyvaSplideJs::js/splide.min.js';
-    const URL_SPLIDE_MIN_CSS = 'Blackbird_HyvaSplideJs::css/splide.min.css';
-    const HYVA_SPLIDE_JS = 'hyva_splide_js';
+    public const HYVA_SPLIDE_JS = 'hyva_splide_js';
 }
---
+++ Api/RequirementsInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blackbird\HyvaSplideJs\Api;
+
+interface RequirementsInterface
+{
+    public const URL_SPLIDE_MIN_JS = 'Blackbird_HyvaSplideJs::js/splide.min.js';
+    public const URL_SPLIDE_MIN_CSS = 'Blackbird_HyvaSplideJs::css/splide.min.css';
+
+    public const FILE_NAME_SPLIDE_MIN_JS = 'splide.min.js';
+}
